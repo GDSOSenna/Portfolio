45428b1bc02d5ccedcb30bbbf382e27204011b8f
Gustavo Senna Duarte Oliveira <gustavo.duarte@fleetcor.com.br>
2024-02-07 10:01:34 -0300

    SideBarLeft components :construction:

---
+++ src/components/IconsBar.js
@@ -0,0 +1,7 @@
+function IconsBar ({source, type, title}){
+    return (
+        <img src={source} alt={type} title={title}/>
+    )
+}
+
+export default IconsBar;
--- src/components/SideBarLeft.js
+++ src/components/SideBarLeft.js
@@ -1,4 +1,8 @@
 import List from "./List";
+import IconsBar from "./IconsBar";
+import Instagram from "./svg/svgInstagram.svg";
+import LinkedIn from "./svg/svgLinkedin.svg";
+import Github from "./svg/svgGithub.svg"
 
 function SideBarLeft(){
     return(
@@ -12,6 +16,11 @@ function SideBarLeft(){
                 <List item="WORK"/>
                 <List item="CONTACT"/>
             </ul>
+            <div>
+                <IconsBar source={Instagram} type="Instagram" title="Instagram"/>
+                <IconsBar source={Github} type="Github" title="Github"/>
+                <IconsBar source={LinkedIn} type="LinkedIn" title="LinkedIn"/>
+            </div>
         </header>
     )
 }
